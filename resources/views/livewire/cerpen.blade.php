34d01fc8d7507df27198c3e5e31c09041badbe9b
manzapo <manzapo@users.noreply.github.com>
2025-02-12 12:25:01 +0700

    Juknis Cerpen Digital

--- resources/views/livewire/cerpen.blade.php
+++ resources/views/livewire/cerpen.blade.php
@@ -1,12 +1,94 @@
 <div>
 	<x-slot:pageid>cerpen</x-slot>
 
-	<div class="container">
-		<div class="row justify-content-xxl-center">
-			<div class="col-xxl-6">
-				<h1>Pendahuluan</h1>
+	<div class="container mb-5">
+		<div class="row justify-content-md-center">
+			<div class="col-md-9 col-lg-7 col-xxl-6">
+				<h2>Pendahuluan</h2>
 				<p>Cerita pendek atau disingkat "cerpen" merupakan sebuah kisah cerita yang ditulis secara singkat, berpusat pada satu peristiwa baik nyata maupun fiksi, dan berpusat pada satu tokoh, sehingga membentuk narasi tunggal. Cerpen diceritakan kepada orang lain dengan tujuan memberi edukasi moral, religi, ataupun hiburan.</p>
 				<p>Mempresentasikan cerita pendek dengan media yang tepat dapat meningkatkan daya tarik dan pemahaman audiens terhadap cerita.</p>
+				<h2>Syarat Peserta</h2>
+				<ol>
+					<li>Peserta adalah siswa/i kelas VII, VIII dan IX SMP/MTs se-Eks Karesidenan Madiun dan Sekolah yang diundang dengan membawa kartu pelajar.</li>
+					<li>Perlombaan bersifat individu.</li>
+					<li>Peserta wajib membayar biaya pendaftaran sebesar Rp. 20.000. Melalui no rekening: BRI 041201118420509 Atas Nama (a.n Moch. In'am Rahmani,) atau BSI 7129635589 Atas Nama (a.n Eka Pratama E.,)</li>
+					<li>Peserta harus mengirimkan media presentasi sebagai pendukung mempresentasikan cerpen.</li>
+					<li>Setiap madrasah atau sekolah bisa mendaftarkan putra-putri terbaiknya tanpa batasan maksimal.</li>
+					<li>Peserta belum pernah menjadi juara 1 pada lomba serupa (presentasi cerita pendek) yang diselenggarakan oleh MAN 1 Ponorogo pada tahun-tahun sebelumnya.</li>
+					<li>Media presentasi yang dikirimkan wajib mengikuti tema materi cerita atau ketentuan yang telah ditetapkan dalam ketentuan lomba.</li>
+					<li>Peserta mengupload twibbon MANCITOSH, dengan format:
+						<ul>
+							<li>Slide pertama : Twibbon</li>
+							<li>Slide kedua : Poster</li>
+						</ul>
+					</li>
+					<li>Twibbon dan caption dapat diunduh melalui link berikut: <a href="https://www.twibbonize.com/mancitoshv9">twibbonize.com/mancitoshv9</a></li>
+					<li>Twibbon diunggah di Instagram dan tag akun <a href="https://instagram/man1ponorogo">@man1ponorogo</a> dan <a href="https://instagram.com/osmanza.po">@osmanza.po</a></li>
+					<li>Link pendaftaran lomba Cerpen berbantuaan aplikasi presentasi: <a href="https://bit.ly/PendaftaranMancitoshv9">bit.ly/PendaftaranMancitoshv9</a></li>
+				</ol>
+				<h2>Ketentuan Cerpen Berbantuan Aplikasi Presentasi</h2>
+				<ol>
+					<li>Setiap peserta wajib datang 30 menit sebelum lomba dimulai.</li>
+					<li>Menggunakan perangkat lunak :
+						<ul>
+							<li>Micrososft PowerPoint</li>
+							<li>Google Slides</li>
+							<li>Canva</li>
+							<li>Apple Keynote</li>
+						</ul>
+					</li>
+					<li>Tidak boleh menyalin karya orang lain.</li>
+					<li>Tidak boleh memakai template.</li>
+					<li>Proses pembuatan semuanya dimulai dari Nol waktu perlombaan berlangsung.</li>
+					<li>Presentasi berdurasi maksimal 8 menit dihitung sejak peserta mengucap kata pertama.</li>
+					<li>Presentasi dapat menggunakan musik dan efek suara.</li>
+					<li>Pendaftaran dibuka pada tanggal 6 Februari 2025 dan ditutup pada tanggal 18 Februari 2025 pukul 15.00 WIB.</li>
+					<li>Karya dikumpulkan pada Link pengumpulan yang akan di-<em>share</em> oleh panitia. Karya dikumpulkan pada tanggal 18 Februari – 20 Februari 2025. Batas akhir pengumpulan karya tanggal 20 Februari 2025 pukul 22.00 WIB.</li>
+					<li>Peserta yang telah menyelesaikan pendaftaran dan mengumpulkan karya akan langsung di undang sebagai finalis.</li>
+					<li>Segala keperluan untuk babak selanjutnya (laptop, charger, dan konverter proyektor) menjadi tanggung jawab peserta.</li>
+					<li>Seluruh karya media presentasi untuk mendukung presentasi cerita pendek yang masuk ke panitia menjadi hak milik panitia.</li>
+					<li>Apabila peserta melakukan kecurangan dalam perlombaan maka panitia berhak melakukan diskualifikasi terhadap peserta tersebut.</li>
+				</ol>
+				<h2>Kriteria Penilaian</h2>
+				<ol>
+					<li>Konten (40%):
+						<ul>
+							<li>Kesesuaian tema, kejelasan dan pendalaman materi.</li>
+						</ul>
+					</li>
+					<li>Desain dan tampilan (30%):
+						<ul>
+							<li>Kreativitas, estetika dan keserasian.</li>
+						</ul>
+					</li>
+					<li>Presentasi (20%):
+						<ul>
+							<li>Komunikasi efektif, penggunaan teknologi dan waktu.</li>
+						</ul>
+					</li>
+					<li>Kreativitas dan originalitas (10%):
+						<ul>
+							<li>Keunikan dan inovasi.</li>
+						</ul>
+					</li>
+				</ol>
+				<h2>Tanggal Penting Cerpen Berbantuan Aplikasi Presentasi</h2>
+				<ul>
+					<li>06 Februari - 18 Februari 2025 : Pendaftaran</li>
+					<li>17 Februari 2025 : Technical Meeting</li>
+					<li>18 Februari - 20 Februari 2025 : Pengumpulan Karya</li>
+					<li>22 Februari 2025 : Perlombaan dan Pengumuman Kejuaraan</li>
+				</ul>
+				<h2>Ketentuan lain</h2>
+				<ol>
+					<li>Keputusan dewan juri bersifat MUTLAK dan tidak dapat diganggu gugat.</li>
+					<li>Hal-hal yang belum tercantum akan dalam juklak juknis akan disampaikan panitia pada Technical Meeting.</li>
+				</ol>
+				<h2>Contact person</h2>
+				<ul>
+					<li>Aditya : 085810310796</li>
+					<li>Dea : 0858581295</li>
+				</ul>
 			</div>
 		</div>
 	</div>
