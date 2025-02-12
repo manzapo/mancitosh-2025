89f0c8cbb708a5b39c51526610b18a673ec02e54
manzapo <manzapo@users.noreply.github.com>
2025-02-12 14:21:12 +0700

    Juknis Tenis Meja

--- resources/views/livewire/tenismeja.blade.php
+++ resources/views/livewire/tenismeja.blade.php
@@ -1,3 +1,69 @@
 <div>
 	<x-slot:pageid>tenismeja</x-slot>
+
+	<div class="container mb-5">
+		<div class="row justify-content-md-center">
+			<div class="col-md-9 col-lg-7 col-xxl-6">
+				<h2>Syarat Peserta</h2>
+				<ol>
+					<li>Peserta adalah siswa/i kelas VII, VIII dan IX SMP/MTs se-Eks Karesidenan Madiun dan Sekolah yang diundang dengan membawa kartu pelajar.</li>
+					<li>Perlombaan bersifat individu.</li>
+					<li>Peserta wajib membayar biaya pendaftaran sebesar Rp.20.000. Melalui no rekening: BRI 041201118420509 Atas Nama (a.n Moch. In'am Rahmani,) atau BSI 7129635589 Atas Nama (a.n Eka Pratama E.,)
+					Peserta mengupload twibbon MANCITOSH, dengan format:
+						<ul>
+							<li>Slide pertama : Twibbon</li>
+							<li>Slide kedua : Poster</li>
+						</ul>
+					</li>
+					<li>Twibbon dan caption dapat diunduh melalui link berikut: <a href="https://www.twibbonize.com/mancitoshv9">twibbonize.com/mancitoshv9</a></li>
+					<li>Twibbon diunggah di Instagram dan tag akun <a href="https://instagram/man1ponorogo">@man1ponorogo</a> dan <a href="https://instagram.com/osmanza.po">@osmanza.po</a></li>
+					<li>Link pendaftaran lomba Tenis meja : <a href="https://bit.ly/PendaftaranMancitoshv9">bit.ly/PendaftaranMancitoshv9</a></li>
+				</ol>
+				<h2>Peraturan Pertandingan Tenis Meja</h2>
+				<ol>
+					<li>Tenis meja tunggal</li>
+					<li>Pemain tidak boleh digantikan oleh pemain lain selama pertandingan berlangsung.</li>
+					<li>Setiap sekolah di perbolehkan mengirimkan lebih dari 1 perwaklian (bebas)</li>
+					<li>Pertandingan dipimpin oleh wasit dan wasit pembantu serta dibantu oleh pencatat skor yang disediakan dari panitia (netral).</li>
+					<li>Wasit memiliki keputusan mutlak dan tidak bisa diganggu gugat.</li>
+					<li>Peserta harus dalam keadaan sehat jasmani dan rohani.</li>
+					<li>Toleransi keterlambatan 10 menit.</li>
+					<li>Pemanggilan peserta 3 x, apabila tidak memenuhi panggilan maka didiskualifikasi.</li>
+					<li>Saat bertanding pemain wajib mengenakan sepatu.</li>
+					<li>Perlombaan di bagi menjadi 2 kejuaraan tunggal (PA) dan tunggal (PI)</li>
+				</ol>
+				<h2>Proses Pertandingan</h2>
+				<ol>
+					<li>Undian untuk menentukan siapa yang melakukan serve pertama.</li>
+					<li>Pertandingan di mulai dengan serve.</li>
+					<li>Pemain bergantian memukul bola.</li>
+					<li>Set berlangsung selama 11 poin. Suatu game dinyatakan dimenangkan oleh seorang pemain yang pertama mendapat poin 11, kecuali pemain sama mendapat poin 10, pada situasi ini, salah satu pemain harus mendapat selisih kemenangan 2 poin atas lawannya.</li>
+					<li>Pertandingan terdiri dari 3-5 set</li>
+				</ol>
+				<h2>Sanksi</h2>
+				<ol>
+					<li>Pemain yang tidak mematuhi aturan dapat di diskualifikasi.</li>
+					<li>Pemanggilan peserta 3 x, apabila tidak memenuhi panggilan maka didiskualifikasi.</li>
+					<li>Pemain yang melakukan pelanggaran lainya akan di berikan peringatan.</li>
+				</ol>
+				<h2>Protes</h2>
+				<ol>
+					<li>Protes hanya dapat diajukan oleh pemain atau pelatih.</li>
+					<li>Keputusan panitia tidak dapat diganggu gugat.</li>
+				</ol>
+				<h2>Tanggal Penting Tenis Meja</h2>
+				<ul>
+					<li>06 Februari - 18 Februari 2025 : Pendaftaran</li>
+					<li>17 Februari 2025 : Technical Meeting</li>
+					<li>21 Februari 2025 : Perlombaan</li>
+					<li>22 Februari 2025 : Final Lomba dan Pengumuman Kejuaraan</li>
+				</ul>
+				<h2>Contact Person</h2>
+				<ul>
+					<li>Abi : 0895800780909</li>
+					<li>Ramzi : 081249854730</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
