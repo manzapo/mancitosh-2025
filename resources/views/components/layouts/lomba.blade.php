6dc206df91eaa9c6f75631cd5718fed4401f2d78
manzapo <manzapo@users.noreply.github.com>
2025-02-12 14:55:36 +0700

    Navbar Lomba responsive

--- resources/views/components/layouts/lomba.blade.php
+++ resources/views/components/layouts/lomba.blade.php
@@ -17,28 +17,27 @@
 				<a class="navbar-brand" href="{{ route('home') }}">
 					<img src="{{ asset('images/navbar-brand.png') }}">
 				</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav ms-auto">
-						<li class="nav-item">
+						<li class="nav-item text-center">
 							<a href="{{ route('home') }}" class="nav-link" wire:navigate>Beranda</a>
 						</li>
-						<li class="nav-item">
+						<li class="nav-item text-center">
 							<a href="{{ route('vlog') }}" class="nav-link" id="navVlog" wire:navigate>Vlog</a>
 						</li>
-						<li class="nav-item">
+						<li class="nav-item text-center">
 							<a href="{{ route('pai') }}" class="nav-link" id="navPai" wire:navigate>Olimpiade PAI</a>
 						</li>
-						<li class="nav-item">
+						<li class="nav-item text-center">
 							<a href="{{ route('tenismeja') }}" class="nav-link" id="navTenismeja" wire:navigate>Tenis Meja</a>
 						</li>
-						<li class="nav-item">
+						<li class="nav-item text-center">
 							<a href="{{ route('poster') }}" class="nav-link" id="navPoster" wire:navigate>Desain Poster</a>
 						</li>
-						<li class="nav-item">
+						<li class="nav-item text-center">
 							<a href="{{ route('cerpen') }}" class="nav-link" id="navCerpen" wire:navigate>Cerpen Digital</a>
 						</li>
 					</ul>
