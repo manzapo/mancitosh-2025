818799e8d2a85af4389f8ad962d1f41d258c781b
manzapo <manzapo@users.noreply.github.com>
2025-02-12 14:09:12 +0700

    Juknis Vlog

--- resources/views/livewire/vlog.blade.php
+++ resources/views/livewire/vlog.blade.php
@@ -1,4 +1,74 @@
 <div>
 	<x-slot:pageid>vlog</x-slot>
 
+	<div class="container mb-5">
+		<div class="row justify-content-md-center">
+			<div class="col-md-9 col-lg-7 col-xxl-6">
+				<h2>Syarat Peserta</h2>
+				<ol>
+					<li>Peserta adalah siswa/i kelas VII, VIII dan IX SMP/MTs se-Eks Karesidenan Madiun dan Sekolah yang diundang dengan membawa kartu pelajar.</li>
+					<li>Lomba bersifat individu atau kelompok beranggotakan maksimal 3 siswa.</li>
+					<li>Peserta wajib membayar biaya pendaftaran sebesar Rp. 20.000 (individu) dan Rp. 40.000 (kelompok). Melalui no rekening: BRI 041201118420509 Atas Nama (a.n Moch. In'am Rahmani,) atau BSI 7129635589 Atas Nama (a.n Eka Pratama E.,)</li>
+					<li>Peserta hanya boleh mengirimkan satu karya.</li>
+					<li>Peserta mengupload twibbon MANCITOSH, dengan format:
+						<ul>
+							<li>Slide pertama : Twibbon</li>
+							<li>Slide kedua : Poster</li>
+						</ul>
+					</li>
+					<li>Twibbon dan caption dapat diunduh melalui link berikut: <a href="https://www.twibbonize.com/mancitoshv9">twibbonize.com/mancitoshv9</a></li>
+					<li>Twibbon diunggah di Instagram dan tag akun <a href="https://instagram/man1ponorogo">@man1ponorogo</a> dan <a href="https://instagram.com/osmanza.po">@osmanza.po</a></li>
+					<li>Link pendaftaran lomba Vlog: <a href="https://bit.ly/PendaftaranMancitoshv9">bit.ly/PendaftaranMancitoshv9</a></li>
+				</ol>
+				<h2>Ketentuan Vlog</h2>
+				<ol>
+					<li>Tema lomba Vlog kali ini adalah "KECERDASAN BUATAN UNTUK MASA DEPAN".</li>
+					<li>Durasi vlog minimal 1 menit dan maksimal 3 menit, termasuk intro, inti Vlog dan credit title, jika melebihi durasi akan berdampak pengurangan nilai.</li>
+					<li>Artis dan Aktor adalah siswa, guru atau staf sekolah masing-masing bukan dari pihak instansi, lembaga atau sanggar (kecuali objek manusia yang muncul sebagai latar, contoh: orang-orang di pasar, taman, dll).</li>
+					<li>Peserta diperbolehkan menggunakan aplikasi apapun sesuai kemampuan.</li>
+					<li>Vlog harus dibuat sendiri dan tidak boleh menggunakan template.</li>
+					<li>Aspek rasio video boleh horizontal (16:9) atau vertikal (9:16).</li>
+					<li>Hak cipta musik yang digunakan, disarankan memproduksi sendiri, atau menggunakan musik yang berlisensi free copyright. Kami tidak bertanggungjawab jika dikemudian hari ada klaim hak cipta dari pemilik musiknya.</li>
+					<li>Footage video harus dibuat sendiri dan merupakan karya orisinil peserta.</li>
+					<li>Keputusan dewan juri bersifat mutlak.</li>
+					<li>Jika peserta terbukti melakukan kecurangan dalam bentuk apapun maka panitia berhak mendiskualifikasi.</li>
+					<li>Hasil karya di upload Melalui google drive dengan privasi akses yang terbuka. Dengan ketentuan nama file:
+						<ul>
+							<li>MANCHITOSH V.9 - [Judul video] - [Asal sekolah].</li>
+							<li>Contoh:</li>
+							<li>MANCITOSH V.9 - AKU DAN AI - MTSN 15 PONOROGO</li>
+						</ul>
+					</li>
+					<li>Link video dikirimkan melalui google form berikut ini : <a href="https://drive.google.com/drive/folders/1S0AvTK3WymHYrF1Q4O2K0YwOcSqDiD8A?usp=drive_link">PENGUMPULAN VLOG MANCHITOSH V.9</a></li>
+					<li>Formulir pengiriman karya akan ditutup maksimal pada tanggal 20 Februari 2024 pukul 23:59.</li>
+					<li>Seluruh peserta yang telah mendaftar akan diundang untuk mengikuti rangkaian final pada tanggal 22 Februari 2024 di MAN 1 Ponorogo.</li>
+					<li>Pada saat final, peserta wajib membawa peralatan editing (laptop/Smartphone) dan juga bahan hasil produksinya (video/audio hasil shoting).</li>
+					<li>Di final tidak ada presentasi, melainkan uji kemampuan editing. Yang ketentuannya akan disampaikan oleh dewan juri pada pelaksaan final.</li>
+				</ol>
+				<h2>Kriteria Penilaian</h2>
+				<ol>
+					<li>Aspek Content (40%) Meliputi: kesesuaian dengan tema dan pesan yang komunikatif.</li>
+					<li>Aspek Visual dan Audio (40%) Meliputi: teknik pengambilan gambar dan kualitas Audio serta kreatifitas editing.</li>
+					<li>Keunikan Ide (20%) Meliputi: cerita yang berbeda namun tetap sesuai tema.</li>
+				</ol>
+				<h2>Tanggal Penting Vlog</h2>
+				<ul>
+					<li>06 Februari - 18 Februari 2025 : Pendaftaran</li>
+					<li>17 Februari 2025 : Technical Meeting</li>
+					<li>20 Februari 2025 : Batas Akhir Pengumpulan Karya</li>
+					<li>22 Februari 2025 : Final Lomba dan Pengumuman Kejuaraan</li>
+				</ul>
+				<h2>Ketentuan Lain</h2>
+				<ol>
+					<li>Keputusan dewan juri bersifat mutlak dan tidak diganggu gugat</li>
+					<li>Hal-hal yang belum tercantum akan dalam juklak juknis akan disampaikan panitia pada Technical Meeting</li>
+				</ol>
+				<h2>Contact Person</h2>
+				<ul>
+					<li>Rifqi : 085815413907</li>
+					<li>Rizqi : 083845851885</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
