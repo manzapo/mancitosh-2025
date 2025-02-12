f7ea28b62fb0ef7d56d41794cd6e8ce17479e883
manzapo <manzapo@users.noreply.github.com>
2025-02-12 14:33:03 +0700

    Juknis Olimpiade PAI

--- resources/views/livewire/pai.blade.php
+++ resources/views/livewire/pai.blade.php
@@ -1,4 +1,62 @@
 <div>
 	<x-slot:pageid>pai</x-slot>
 
+	<div class="container mb-5">
+		<div class="row justify-content-md-center">
+			<div class="col-md-9 col-lg-7 col-xxl-6">
+				<h2>Syarat Peserta</h2>
+				<ol>
+					<li>Peserta adalah siswa/i kelas VII, VIII dan IX SMP/MTs se-Eks Karesidenan Madiun dan Sekolah yang diundang dengan membawa kartu pelajar.</li>
+					<li>Perlombaan bersifat individu.</li>
+					<li>Peserta wajib membayar biaya pendaftaran sebesar Rp.20.000. Melalui no rekening: BRI 041201118420509 Atas Nama (a.n Moch. In'am Rahmani,) atau BSI 7129635589 Atas Nama (a.n Eka Pratama E.,)
+					Peserta mengupload twibbon MANCITOSH, dengan format:
+						<ul>
+							<li>Slide pertama : Twibbon</li>
+							<li>Slide kedua : Poster</li>
+						</ul>
+					</li>
+					<li>Twibbon dan caption dapat diunduh melalui link berikut: <a href="https://www.twibbonize.com/mancitoshv9">twibbonize.com/mancitoshv9</a></li>
+					<li>Twibbon diunggah di Instagram dan tag akun <a href="https://instagram/man1ponorogo">@man1ponorogo</a> dan <a href="https://instagram.com/osmanza.po">@osmanza.po</a></li>
+					<li>Link pendaftaran lomba Olimpiade PAI : <a href="https://bit.ly/PendaftaranMancitoshv9">bit.ly/PendaftaranMancitoshv9</a></li>
+				</ol>
+				<h2>Ketentuan Olimpiade PAI</h2>
+				<ol>
+					<li>Setiap peserta wajib datang 30 menit sebelum lomba dimulai.</li>
+					<li>Peserta dilarang membawa catatan dalam bentuk apapun.</li>
+					<li>Peserta akan mengerjakan soal dalam 2 format :
+						<ul>
+							<li>Pilihan ganda : [60 soal]</li>
+							<li>Uraian : [10 soal]</li>
+						</ul>
+					</li>
+					<li>Total waktu pengerjaan : 120 menit</li>
+					<li>Peserta tidak diperkenankan mencontek dan memberi contekan.</li>
+					<li>Apabila ada peserta yang ketahuan membawa catatan ataupun mencontek akan langsung di diskualifikasi oleh panitia.</li>
+					<li>Peserta wajib membawa alat tulis masing - masing dan tidak boleh saling meminjam satu sama lain.</li>
+					<li>Peserta yang sudah selesai mengerjakan sebelum batas waktunya dilarang meninggalkan ruangan.</li>
+				</ol>
+				<h2>Kriteria Penilaian</h2>
+				<ol>
+					<li>Penilaian dilakukan berdasarkan skor:
+						<ul>
+							<li>Pilihan ganda: [benar +1, salah 0, dan kosong 0]</li>
+							<li>Uraian: [benar +4, salah 0 dan kosong 0]</li>
+						</ul>
+					</li>
+				</ol>
+				<h2>Ketentuan Lain</h2>
+				<ol>
+					<li>Keputusan dewan juri bersifat MUTLAK dan tidak dapat diganggu gugat.</li>
+					<li>Juara olimpiade ditentukan berdasarkan nilai yang diperoleh.</li>
+					<li>Juara olimpiade berhak mendapatkan uang pembinaan dan piagam penghargaan.</li>
+					<li>Hal-hal yang belum tercantum akan dalam juklak juknis akan disampaikan panitia pada Technical Meeting.</li>
+				</ol>
+				<h2>Contact Person</h2>
+				<ul>
+					<li>Wildan : 0881026782706</li>
+					<li>Delima : 089652722456</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
