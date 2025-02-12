dc7069ff30289f3cf46dd261b47499f37daa70d4
manzapo <manzapo@users.noreply.github.com>
2025-02-12 14:47:25 +0700

    Juknis Desain Poster

--- resources/views/livewire/poster.blade.php
+++ resources/views/livewire/poster.blade.php
@@ -1,4 +1,94 @@
 <div>
 	<x-slot:pageid>poster</x-slot>
 
+	<div class="container mb-5">
+		<div class="row justify-content-md-center">
+			<div class="col-md-9 col-lg-7 col-xxl-6">
+				<h2>Syarat peserta</h2>
+				<ol>
+					<li>Peserta adalah siswa/i kelas VII, VIII dan IX SMP/MTs se-Eks Karesidenan Madiun dan Sekolah yang diundang dengan membawa kartu pelajar.</li>
+					<li>Perlombaan bersifat individu.</li>
+					<li>Peserta wajib membayar biaya pendaftaran sebesar Rp.20.000. Melalui no rekening: BRI 041201118420509 Atas Nama (a.n Moch. In'am Rahmani,) atau BSI 7129635589 Atas Nama (a.n Eka Pratama E.,)
+					Peserta mengupload twibbon MANCITOSH, dengan format:
+						<ul>
+							<li>Slide pertama : Twibbon</li>
+							<li>Slide kedua : Poster</li>
+						</ul>
+					</li>
+					<li>Twibbon dan caption dapat diunduh melalui link berikut: <a href="https://www.twibbonize.com/mancitoshv9">twibbonize.com/mancitoshv9</a></li>
+					<li>Twibbon diunggah di Instagram dan tag akun <a href="https://instagram/man1ponorogo">@man1ponorogo</a> dan <a href="https://instagram.com/osmanza.po">@osmanza.po</a></li>
+					<li>Link pendaftaran lomba Desain Poster : <a href="https://bit.ly/PendaftaranMancitoshv9">bit.ly/PendaftaranMancitoshv9</a></li>
+				</ol>
+				<h2>Ketentuan Desain Poster</h2>
+				<ol>
+					<li>Karya poster merupakan karya orisinal, bukan karya plagiasi dan belum pernah diikutsertakan dalam perlombaan sebelumnya.</li>
+					<li>Tema poster : "Unity in Diversity: Harmoni dalam Keberagaman Indonesia"</li>
+					<li>Poster dirancang bebas dengan tetap menjaga kesopanan dan tidak menyinggung SARA.</li>
+					<li>Proses pembuatan poster dapat menggunakan berbagai softwere digital seperti, Canva, Coreldraw,Adobe Illustrator, Photoshop maupun lainnya.</li>
+					<li>Poster dikerjakan secara digital, dapat berupa illustrasi digital, vector, maupun mengkombinasikan antar beberapa teknik tersebut.</li>
+					<li>Peserta tidak diperbolehkan menggunakan Artificial Intelligent (Al) dalam proses pembuatan poster.</li>
+					<li>Apabila menggunakan gambar dari internet, harus melampirkan sumber web tersebut pada bagiandeskripsi konsep karya (file PDF).</li>
+					<li>Ukuran karya memenuhi aspek rasio A3.</li>
+					<li>Orientasi kertas wajib Portrait.</li>
+					<li>Karya yang nantinya dikumpulkan adalah 1 Karya JPEG dan 1 File karya asli (.psd, .cdr, .ai, dan sebagainya)</li>
+					<li>Untuk canva menyertakan link collaborative.</li>
+					<li>Nantinya karya poster dikirim ke link pengumpulan karya untuk dinilai oleh juri dan diambil 20 besar terbaik untuk mengikuti tahap final.</li>
+					<li>Untuk juara favorit diambil dari vote terbanyak like postingan hasil karya peserta di akun ig@man1ponorogo. Batas akhir vote pada tanggal 21 Februari 2025 pukul 17.00 WIB.</li>
+					<li>Pada tahap penyisihan dan final masing-masing peserta dibebaskan untuk memakai laptop,handphone, tablet dan lainnya untuk membuat karya.</li>
+					<li>Peserta finalis wajib membawa handphone/laptop/tablet untuk menuju tahap final dan memastikanbahwa memiliki paket data yang cukup untuk mengakses internet.</li>
+					<li>Ketentuan pelaksanaan final akan disampaikan pada pengumuman selanjutnya.</li>
+				</ol>
+				<h2>Registrasi & Pengumpulan Karya Poster</h2>
+				<ol>
+					<li>Karya dikumpulkan pada Link pengumpulan yang akan di-<em>share</em> oleh panitia. Karya dikumpulkan pada tanggal 18 Februari - 20 Februari 2025. Batas akhir pengumpulan karya tanggal 20 Februari 2025 pukul 15.00 WIB.</li>
+					<li>Berkas yang perlu dipersiapkan untuk di- submit pada link Google Form pengumpulan karya meliputi:
+						<ul>
+							<li>File Karya Poster (JPEG)</li>
+							<li>File Asli Karya Poster (.psd, .cdr, .ai, dan sebagainya)</li>
+							<li>File Deskripsi Karya (PDF)</li>
+							<li>Link collaborative (bagi pengguna Canva)</li>
+						</ul>
+					</li>
+				</ol>
+				<h2>Kriteria Penilaian</h2>
+				<p>Adapun kriteria penilaian masing-masing tahap sebagai berikut:</p>
+				<ol>
+					<li>Tahap 1: Penilaian Karya yang dikumpulkan (Bobot 50%)
+						<ul>
+							<li>Kreativitas dan Desain (35%). Evaluasi terhadap keoriginalan konsep dan desain visual poster.</li>
+							<li>Kesesuaian Tema (10%). Sejauh mana poster mencerminkan tema yang ditetapkan untuk lomba.</li>
+							<li>Pesan dan Komunikasi (25%). Kejelasan dan efektivitas dalam menyampaikan pesan melalui elemenelemen visual dan teks.</li>
+							<li>Kualitas Eksekusi (30%). Kebersihan, kerapihan, dan kualitas keseluruhan eksekusi poster.</li>
+						</ul>
+					</li>
+					<li>Tahap 2: Final (Bobot 50%)
+						<ul>
+							<li>Estetika (40%). Meliputi: Keindahan tampilan karya yang meliputi unsur-unsur visual, seperti bentuk,warna, komposisi, dan tipografi.</li>
+							<li>Kemampuan IT (40%). Meliputi: Pemahaman terhadap kemampuan IT terkait pembuatan karya.</li>
+							<li>Originalitas (20%). Meliputi: Keaslian karya yang terdiri dari konsep hingga eksekusi.</li>
+						</ul>
+					</li>
+				</ol>
+				<h2>Tanggal Penting Desain Poster</h2>
+				<ul>
+					<li>06 Februari - 18 Februari 2025 : Pendaftaran</li>
+					<li>17 Februari 2025 : Technical Meeting</li>
+					<li>18 Februari - 20 Februari 2025 : Pengumpulan Karya</li>
+					<li>20 Februari - 21 Februari 2025 : Vote Juara Favorit</li>
+					<li>21 Februari 2025 ( 17.00 WIB) : Pengumuman Peserta yang lolos Final dan batas Terakhir Vote</li>
+					<li>22 Februari 2025 : Final dan Pengumuman Kejuaraan</li>
+				</ul>
+				<h2>Ketentuan Lain</h2>
+				<ol>
+					<li>Keputusan dewan juri bersifat MUTLAK dan tidak dapat diganggu gugat.</li>
+					<li>Hal-hal yang belum tercantum akan dalam juklak juknis akan disampaikan panitia pada Technical Meeting.</li>
+				</ol>
+				<h2>Contact Person</h2>
+				<ul>
+					<li>Happy : 089524151273</li>
+					<li>Honey : 083874501742</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
